822a774817644ee6cfbdacccb0a2ef7db4111f35
denis.shediakov <luckyswd@gmail.com>
2023-03-15 22:27:04 +0300

    footer and fonts

--- wp-app/wp-content/themes/leonord/hooks_filters/after_setup_theme.php
+++ wp-app/wp-content/themes/leonord/hooks_filters/after_setup_theme.php
@@ -8,8 +8,8 @@ add_action('after_setup_theme', function () {
     register_nav_menus([
         'header_menu_desktop' => 'Меню хедер desktop',
         'header_menu_mobile' => 'Меню хедер mobile',
-        'footer_catalog_left' => 'Меню футер каталог левое',
-        'footer_catalog_right' => 'Меню футер каталог правое',
+        'footer_catalog_col_1' => 'Меню футер 1',
+        'footer_catalog_col_2' => 'Меню футер 2',
     ]);
     add_theme_support(
         'custom-logo',
