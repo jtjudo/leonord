1063d5088e2d59656ac1a516769a45fd508c4976
alexander.olhovik <alexander.olhovik@intexsoft.by>
2023-03-19 22:56:51 +0300

    added about us section

---
+++ wp-app/wp-content/themes/leonord/modules/about-company/about-company.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.about-company__slider', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
--- wp-app/wp-content/themes/leonord/modules/about-company/about-company.php
+++ wp-app/wp-content/themes/leonord/modules/about-company/about-company.php
@@ -8,13 +8,53 @@ Mode: preview
 ?>
 
 <?php
-$test = get_field('test');
+$title = get_field('title');
+$description = get_field('description');
+$link = get_field('link');
+$gallery = get_field('gallery');
+$secondLogo = get_field('second_logo', 'option');
 ?>
 
 <?php if (!is_admin()) : ?>
     <section class="about-company">
-        <div class="container">
-            testqzz
+        <div class="container-xs">
+            <img
+                class="about-company__logo"
+                src="<?= !empty($secondLogo) ? $secondLogo['url'] : ''  ?>"
+                alt="логотип"
+                width="210"
+                height="210"
+            >
+            <div class="about-company__wrapper">
+                <div class="about-company__left">
+                    <?php if (!wp_is_mobile()) : ?>
+                        <h2 class="about-company__title heading"><?= $title ?? '' ?></h2>
+                    <?php endif; ?>
+                    <div class="about-company__slider">
+                        <div class="swiper-wrapper">
+                            <?php foreach ($gallery as $item) : ?>
+                                <div class="swiper-slide about-company__item">
+                                    <img src="<?= $item['url'] ?>" alt="<?= $item['alt'] ?>">
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="swiper-pagination"></div>
+                    </div>
+                </div>
+                <div class="about-company__description-wrapper">
+                    <?php if (wp_is_mobile()) : ?>
+                        <h2 class="about-company__title heading"><?= $title ?? '' ?></h2>
+                    <?php endif; ?>
+                <?= $description ?>
+                <a
+                    class="about-company__link button"
+                    href="<?= $link['url'] ?>"
+                    <?= $link['target'] ?>
+                >
+                    <?= $link['title'] ?>
+                </a>
+            </div>
+            </div>
         </div>
     </section>
 <?php else: ?>
